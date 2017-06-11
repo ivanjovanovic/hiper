3f44cb595b1cfa3f26acf7018a9e0683089c0e91
Ivan Jovanovic <ivan.jovanovic@gmail.com>
2017-06-11 08:49:55 +0200

    Select configuration file from provided paths

--- src/Data/Hiper.hs
+++ src/Data/Hiper.hs
@@ -14,10 +14,12 @@ module Data.Hiper
     , lookup
     ) where
 
+import System.Directory
+import Control.Monad
 import Data.IORef
-import Data.Text
+import Data.Text hiding (take)
 import Control.Monad (join)
-import Prelude hiding (lookup)
+import Prelude hiding (lookup, concat, FilePath)
 import qualified Data.Map.Lazy as M
 import Data.Hiper.Types.Internal
 import Data.Hiper.Instances ()
@@ -31,6 +33,8 @@ type Name = Text
 -- | Extension of the config file
 type Extension = Text
 
+type FilePath = Text
+
 -- | Hiper configuration
 data HiperConfig = HiperConfig {
     hcPaths :: [Path]
@@ -39,11 +43,13 @@ data HiperConfig = HiperConfig {
   , hcDefaults :: M.Map Name Value
   }
 
+-- | Provides an empty config to build on top.
+-- Contains default list of supported extensions
 emptyConfig :: HiperConfig
 emptyConfig = HiperConfig
   { hcPaths = []
   , hcFile = ""
-  , hcExtensions = []
+  , hcExtensions = ["json", "yaml"]
   , hcDefaults = M.empty
   }
 
@@ -65,11 +71,34 @@ data Hiper = Hiper {
 loadConfig :: HiperConfig -> IO Hiper
 loadConfig c = do
   mapIORef <- newIORef (hcDefaults c)
+  -- Find configuration file from the configured paths
+
 
   -- check if there are ENV variables with the same names to take over defaults
   return $ Hiper mapIORef c
 
+-- | lookup allows getting the value from the config registry.
+-- It lets the caller specify return type
 lookup :: Convertible a => Hiper -> Name -> IO (Maybe a)
 lookup (Hiper values config) name = do
   valueMap <- readIORef values
   return $ join $ fmap convert (M.lookup name valueMap)
+
+-- | Searches for configuration files and returns path
+-- to the first one found according to configuration
+-- Returns Nothing for non-complete configuration.
+configFilePath :: HiperConfig -> IO (Maybe FilePath)
+configFilePath (HiperConfig [] _ _ _) = return Nothing
+configFilePath (HiperConfig _"" _ _) = return Nothing
+configFilePath (HiperConfig _ _ [] _) = return Nothing
+configFilePath (HiperConfig paths file extensions _) = do
+  potentialFiles <- filterM (doesFileExist . unpack) (filePaths paths extensions file)
+  case take 1 potentialFiles of
+    [] -> return Nothing
+    [f] -> return (Just f)
+
+filePaths :: [Path] -> [Extension] -> Name -> [FilePath]
+filePaths paths extensions name = do
+  path <- paths
+  extension <- extensions
+  return $ concat [path, pack "/", name, pack ".", extension]
